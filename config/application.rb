ee35255f70ce7a35f53b6a36d74efd881f2fce1b
kouuuki <mark.web.888@gmail.com>
2018-06-07 11:00:39 +0900

    add com

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Miuraspace
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.active_record.raise_in_transactional_callbacks = true
 
     config.generators do |g|
